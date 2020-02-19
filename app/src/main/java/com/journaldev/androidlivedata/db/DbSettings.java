0e573d1d3802e736812d5612e0a094b1904e7ea9
Soumit Kolay <soumit.wgt@gmail.com>
2020-02-19 16:35:14 +0530

    LiveData first push

---
+++ app/src/main/java/com/journaldev/androidlivedata/db/DbSettings.java
@@ -0,0 +1,16 @@
+package com.journaldev.androidlivedata.db;
+
+import android.provider.BaseColumns;
+
+public class DbSettings {
+
+    static final String DB_NAME = "favourites.db";
+    static final int DB_VERSION = 1;
+
+    public class DBEntry implements BaseColumns {
+        public static final String TABLE = "fav";
+        public static final String COL_FAV_URL = "url";
+        public static final String COL_FAV_DATE = "date";
+    }
+
+}
